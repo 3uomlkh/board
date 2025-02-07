fb03939b1aaf3bcb002706f85ebf579956112dde
3uomlkh <3uomlkh@gmail.com>
2025-02-07 18:10:19 +0900

    feat: 특정 게시글 조회 기능 구현

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.example.board.service;
 
 import com.example.board.dto.BoardResponseDto;
+import com.example.board.dto.BoardWithAgeResponseDto;
 import com.example.board.entity.Board;
 import com.example.board.entity.Member;
 import com.example.board.repository.BoardRepository;
@@ -33,5 +34,11 @@ public class BoardService {
                 .stream()
                 .map(BoardResponseDto::toDto)
                 .toList();
+    }
+
+    public BoardWithAgeResponseDto findById(Long id) {
+        Board findBoard = boardRepository.findByIdOrElseThrow(id);
+        Member member = findBoard.getMember();
+        return new BoardWithAgeResponseDto(findBoard.getTitle(), findBoard.getContents(), member.getAge());
     }
 }
