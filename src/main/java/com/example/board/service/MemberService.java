7875090753f675cb81c003ee812831dd7207728f
3uomlkh <3uomlkh@gmail.com>
2025-02-07 16:34:56 +0900

    feat: 특정 회원 조회 기능 구현

--- src/main/java/com/example/board/service/MemberService.java
+++ src/main/java/com/example/board/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.example.board.service;
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.entity.Member;
 import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +23,18 @@ public class MemberService {
         Member saveMember = memberRepository.save(member);
 
         return new SignUpResponseDto(saveMember.getId(), saveMember.getUserName(), saveMember.getAge());
+    }
+
+    public MemberResponseDto findById(Long id) {
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exists id = " + id);
+        }
+
+        Member findMember = optionalMember.get();
+
+        return  new MemberResponseDto(findMember.getUserName(), findMember.getAge());
     }
 }
