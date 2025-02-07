88b7e63fe620989c411979b296f1ce4edc0e9a1b
3uomlkh <3uomlkh@gmail.com>
2025-02-07 15:11:43 +0900

    feat: Board Entity 구현

---
+++ src/main/java/com/example/board/entity/Board.java
@@ -0,0 +1,17 @@
+package com.example.board.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "board")
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String contents;
+}
